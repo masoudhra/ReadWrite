9a1e1c6e3cd3cd9f4f6a211889632d5b618edbb6
Masoud Ramezani <masoudhra@gmail.com>
2023-09-06 16:48:50 +1200

    append with a and rewrite with w

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # moddhaye mojoo : r read  w write  a append  r+ read and write
-films_file = open("films.text", "r")
+# films_file = open("films.text", "r")
 
-films = films_file.readlines()
-for film in films:
-    print(film)
+# films = films_file.readlines()
+# for film in films:
+#     print(film)
 
-films_file.close() # hatman bayad bebandimesh
+# films_file.close() # hatman bayad bebandimesh
 
+
+films_file = open("films.text", "a")
+films_file.write("\nThis is the file for the movies")
+films_file.close()
+
+# agar w bezarim va faghat write konim neveshtehaye ghabli pak mishe va agghat chizi ke khastim ezafe mishe
